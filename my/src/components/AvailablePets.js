216803b340b28d6c2bbb2d0e1c9750c407999764
prohansai <rohanproddaturi2005@gmail.com>
2025-04-11 14:40:37 +0530

    Update API endpoints to use production backend URL

--- my/src/components/AvailablePets.js
+++ my/src/components/AvailablePets.js
@@ -12,7 +12,7 @@ const AvailablePets = () => {
   useEffect(() => {
     const fetchPets = async () => {
       try {
-        const response = await fetch('http://localhost:5000/api/pets'); 
+        const response = await fetch('https://petsheaven-backend.onrender.com/api/pets'); 
         const data = await response.json();
         setPets(data);
       } catch (error) {
